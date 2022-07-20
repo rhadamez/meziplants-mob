8bb064612e1f7549192b7a7c3bb86a921f386b15
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-07-19 23:08:39 -0300

    feat: storing user name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useEffect, useState } from 'react'
+
 import * as S from './styles'
 
 export function Header() {
+	const [name, setName] = useState('')
+
+	useEffect(() => {
+		async function loadName() {
+			const nameExists = await AsyncStorage.getItem('@meziplants:name')
+			if(nameExists) setName(nameExists)
+		}
+
+		loadName()
+	}, [])
+
 	return (
 		<S.Container>
 			<S.ProfileName>
-				<S.Greeting>Olá</S.Greeting>
-				<S.Username>Rhadamez</S.Username>
+				<S.Greeting>Olá,</S.Greeting>
+				<S.Username>{name}</S.Username>
 			</S.ProfileName>
 
 			<S.Avatar source={{ uri: 'https://github.com/rhadamez.png'}}/>
--- src/screens/UserIdentification/index.tsx
+++ src/screens/UserIdentification/index.tsx
@@ -1,12 +1,21 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useNavigation } from '@react-navigation/native'
 import { useState } from 'react'
-import { Keyboard } from 'react-native'
+import { Alert, Keyboard } from 'react-native'
 import { Button } from '../../components/Button'
 import * as S from './styles'
 
 export function UserIdentification() {
 	const [name, setName] = useState('')
 	const navigation = useNavigation()
+
+	async function handleSubmitName() {
+		if (!name) return Alert.alert('Me diz como chamar você 😉')
+
+		await AsyncStorage.setItem('@meziplants:name', name)
+
+		navigation.navigate('Confirmation', { name })
+	}
 
 	return (
 		<S.KeyboardView behavior={{ ios: 'padding' }} style={{ flex: 1 }}>
@@ -16,14 +25,14 @@ export function UserIdentification() {
 						<S.Form>
 							<S.Header>
 								<S.Title>Como podemos{'\n'}chamar você?</S.Title>
-								<S.Emoji>{name.length > 0 ? '😃' : '🙂' }</S.Emoji>
+								<S.Emoji>{name.length > 0 ? '😃' : '🙂'}</S.Emoji>
 								<S.Input
 									value={name}
 									onChangeText={(text: string) => setName(text)}
-									placeholder='Digite um nome'/>
+									placeholder='Digite um nome' />
 							</S.Header>
 							<S.Footer>
-								<Button title='Confirmar' onPress={() => navigation.navigate('Confirmation', { name })} />
+								<Button title='Confirmar' onPress={handleSubmitName} />
 							</S.Footer>
 						</S.Form>
 					</S.Content>
